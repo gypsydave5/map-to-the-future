1616e47f563c7e601c4e3c6f2993027934276cfa
binaryberry <tatienantes@yahoo.fr>
2014-10-13 12:11:14 +0100

    added categories to events.rb

--- lib/model/event.rb
+++ lib/model/event.rb
@@ -4,10 +4,10 @@ class Event
 
 include DataMapper::Resource
 
-  # This block describes what resources our model will have
-  property :id,     		Serial # Serial means that it will be auto-incremented for every record
+  property :id,     		Serial 
   property :title,  		String
   property :description,    Text
+  property :category,    	String
   property :date,    		DateTime
   property :geometry,    	Object
 
