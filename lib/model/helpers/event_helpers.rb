2e950ece664d8cfa37370f2d2dd92b1208cfa12d
David Wickes <dave.wickes@gmail.com>
2014-10-16 09:28:53 +0100

    refactors the refactoring as it wasn't quite there the first time.

--- lib/model/helpers/event_helpers.rb
+++ lib/model/helpers/event_helpers.rb
@@ -5,6 +5,47 @@ module EventHelpers
   end
 
   module EventClassHelpers
+
+    def add_geojson_events(geojson)
+      upload = JSON.parse(geojson)
+      upload["features"].each do |feature|
+        event = { geometry: feature["geometry"] }
+        feature["properties"].keys.each do |property|
+          import_property(feature, property, event)
+        end
+        Event.create(event)
+      end
+    end
+
+    def import_date_property(feature, property, event)
+      event[property.to_sym] = DateTime.new(feature["properties"][property].to_i)
+    end
+
+    def import_tag(feature, property, event)
+      event[property.to_sym] = feature["properties"][property].map do |tag|
+        Tag.first_or_create(name: tag)
+      end
+    end
+
+    def import_linked_events(feature, linked_events, event)
+      event[linked_events.to_sym] =
+        feature["properties"][linked_events].map do |linked_event|
+          Event.get(linked_event["id"].to_i)
+        end
+    end
+
+    def import_property(feature, property, event)
+      if property =~ /date/
+        import_date_property(feature, property, event)
+      elsif property == "tags"
+        import_tag(feature, property, event)
+      elsif property == "events"
+        import_linked_events(feature, property, event)
+      else
+        event[property.to_sym] = feature["properties"][property]
+      end
+    end
+
     def fetch_relevant_events(year)
       all({
         :startdate.gte => DateTime.new(year.to_i),
@@ -58,43 +99,5 @@ module EventHelpers
   end
 
 
-  def self.add_geojson_events(geojson)
-    upload = JSON.parse(params[:geoJSON][:tempfile].read)
-    upload["features"].each do |feature|
-      event = { geometry: feature["geometry"] }
-      feature["properties"].keys.each do |property|
-        import_property(property, event)
-      end
-      Event.create(event)
-    end
-  end
 
-  def import_property(property, event)
-    if property =~ /date/
-      import_date_property(property, event)
-    elsif property == "tags"
-      import_tag(property, event)
-    elsif property == "events"
-      import_linked_events(property, event)
-    else
-      event[property.to_sym] = feature["properties"][key]
-    end
-  end
-
-  def import_date_property(property, event)
-    event[property.to_sym] = DateTime.new(feature["properties"][property].to_i)
-  end
-
-  def import_tag(property, event)
-    event[property.to_sym] = feature["properties"][property].map do |tag|
-      Tag.first_or_create(name: tag)
-    end
-  end
-
-  def import_linked_events(linked_events, event)
-    event[linked_events.to_sym] =
-      feature["properties"][linked_events].map do |linked_event|
-        Event.get(linked_event["id"].to_i)
-      end
-  end
 end
