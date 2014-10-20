0d937095ba7935e9893d95ca4199367d2c045afd
David Wickes <dave.wickes@gmail.com>
2014-10-20 17:23:22 +0100

    Trying to fix the tests for moving the slider

--- features/step_definitions/map_events_steps.rb
+++ features/step_definitions/map_events_steps.rb
@@ -10,27 +10,28 @@ Given(/^the Timeline app knows The Battle of Dresden Anniversary$/) do
 end
 
 When(/^I move the slider to "(.*?)"$/) do |year|
-  sleep(5)
+  sleep(3)
   page.execute_script("$('#slider').slider('value', #{year})")
-  sleep(5)
+  page.execute_script("$('.ui-slider-handle').click()")
+  sleep(3)
 end
 
 When(/^I click on the marker$/) do
-  sleep(5)
+  sleep(3)
   page.execute_script("eventLayer.eachLayer(function(marker) { marker.openPopup() });")
-  sleep(5)
+  sleep(3)
 end
 
 Then(/^I should see a marker$/) do
-  sleep(5)
+  sleep(3)
   expect(page).to have_css('img.leaflet-marker-icon')
   sleep(5)
 end
 
 Then(/^I should not see a marker$/) do
-  sleep(5)
+  sleep(3)
   expect(page.evaluate_script("Object.keys(eventLayer._layers).length")).to eq(0)
-  sleep(5)
+  sleep(3)
 end
 
 def point_event(name, description, tags, start_date, end_date, timescale, coords, events)
