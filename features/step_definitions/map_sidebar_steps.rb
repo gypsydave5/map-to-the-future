3f1608141207223de48f0b0ca523e32d4dd8fe21
David Wickes <dave.wickes@gmail.com>
2014-10-22 21:48:25 +0100

    Adds failing test for map initializing with no sidebar

--- features/step_definitions/map_sidebar_steps.rb
+++ features/step_definitions/map_sidebar_steps.rb
@@ -4,5 +4,13 @@ Given(/^I have opened the popup for "(.*?)"$/) do |event|
 end
 
 When(/^I click the 'read more' link$/) do
+  sleep(2)
   page.execute_script("$('.sidebar-caller').click()")
+  sleep(2)
+end
+
+Then(/^I should not see the sidebar$/) do
+  sleep(5)
+  expect(page).not_to have_css('#sidebar')
+  sleep(5)
 end
