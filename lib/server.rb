c82ebf18af9b5f4e59d1b8756573890e6b170a02
David Wickes <dave.wickes@gmail.com>
2014-10-24 13:43:58 +0100

    Fixes typo in the server

--- lib/server.rb
+++ lib/server.rb
@@ -9,7 +9,7 @@ class MapToTheFuture < Sinatra::Base
   get '/events' do
     content_type :json
     all_the_events = Event.all
-    change_togfeatures_collection_json(all_the_events)
+    change_to_features_collection_json(all_the_events)
   end
 
   get '/upload' do
